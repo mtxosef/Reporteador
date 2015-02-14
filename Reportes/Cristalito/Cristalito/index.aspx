1e85eb0ae86b294a6ca4a9230e5a67ec05dfbe20
Giovanni Flores <metalrock3@hotmail.es>
2015-02-13 20:32:46 -0600

    Reporte con CrystalReports
    Reporte con o sin parametros

---
+++ Reportes/Cristalito/Cristalito/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Cristalito.index" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304"
+    Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       <asp:Label ID="lblMensaje" runat="server" Text=""></asp:Label> 
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="getReportByParam" />
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Reportes/Cristalito/Cristalito/index.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace Cristalito
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+        }
+        public void getReport(object sender, EventArgs e)
+        {
+           
+                var conn = new SqlConnection("Data Source=GIOVANNI-PC;Initial Catalog=plantas;User Id=sa;Password=");
+                try
+                {
+                    using (var comando = new SqlCommand("SELECT * FROM  [plantas].[dbo].[Usuario]", conn))
+                    {
+                        using (var adaptador = new SqlDataAdapter(comando))
+                        {
+                            var ds = new DataSet();
+                            adaptador.Fill(ds);
+
+                            var reporte = new ReportDocument();
+                            reporte.Load(Server.MapPath("Usuarios.rpt"));
+                            reporte.SetDataSource(ds.Tables[0]);
+                            reporte.DataSourceConnections[0].SetConnection("GIOVANNI-PC", "plantas", "sa", "");
+
+                            CrystalReportViewer1.ReportSource = reporte;
+                        } // end using adaptador
+                    } // end using comando
+                } // end try
+                catch (Exception ex)
+                {
+                    lblMensaje.Text = ex.Message;
+                }
+                finally
+                {
+                    if (conn.State != ConnectionState.Closed)
+                        conn.Close();
+                    conn.Dispose();
+                } 
+        }
+
+        public void getReportByParam(object sender, EventArgs e)
+        {
+            string param = TextBox1.Text;
+            if (param != "")
+            {
+                var conn = new SqlConnection("Data Source=GIOVANNI-PC;Initial Catalog=plantas;User Id=sa;Password=");
+                try
+                {
+                    using (var comando = new SqlCommand("SELECT * FROM  [plantas].[dbo].[Usuario] usr WHERE (usr.idUsuario = " + param + ")", conn))
+                    {
+                        using (var adaptador = new SqlDataAdapter(comando))
+                        {
+                            var ds = new DataSet();
+                            adaptador.Fill(ds);
+
+                            var reporte = new ReportDocument();
+                            reporte.Load(Server.MapPath("Usuarios.rpt"));
+                            reporte.SetDataSource(ds.Tables[0]);
+                            reporte.DataSourceConnections[0].SetConnection("GIOVANNI-PC", "plantas", "sa", "");
+
+                            CrystalReportViewer1.ReportSource = reporte;
+                        } // end using adaptador
+                    } // end using comando
+                } // end try
+                catch (Exception ex)
+                {
+                    lblMensaje.Text = ex.Message;
+                }
+                finally
+                {
+                    if (conn.State != ConnectionState.Closed)
+                        conn.Close();
+                    conn.Dispose();
+                }
+            }
+        }
+    }
+}
